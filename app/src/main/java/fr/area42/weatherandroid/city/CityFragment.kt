95551ceca718114e1440f8e8ce5daede8c89df1c
kallard <contact@allard-kevin.fr>
2019-06-12 12:45:58 +0200

    :sparkles: Introducing new features.
    Création du fragment

---
+++ app/src/main/java/fr/area42/weatherandroid/city/CityFragment.kt
@@ -0,0 +1,16 @@
+package fr.area42.weatherandroid.city
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.area42.weatherandroid.R
+
+class CityFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_city, container, false)
+
+        return view
+    }
+}
