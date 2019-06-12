37fc3ef94a6f53ad69e3f90e62c4d793f506fba8
kallard <contact@allard-kevin.fr>
2019-06-12 12:45:35 +0200

    :truck: Moving file.

--- app/src/main/java/fr/area42/weatherandroid/city/CityActivity.kt
+++ app/src/main/java/fr/area42/weatherandroid/city/CityActivity.kt
@@ -1,7 +1,8 @@
-package fr.area42.weatherandroid
+package fr.area42.weatherandroid.city
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import fr.area42.weatherandroid.R
 
 class CityActivity : AppCompatActivity() {
 
