6c982bd156aea7f0c06ec0ca8b0af0655a38b4ff
kallard <contact@allard-kevin.fr>
2019-06-12 12:34:23 +0200

    :truck: Moving or renaming files.

--- app/src/main/java/fr/area42/weatherandroid/CityActivity.kt
+++ app/src/main/java/fr/area42/weatherandroid/CityActivity.kt
@@ -3,10 +3,10 @@ package fr.area42.weatherandroid
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CityActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.city_main)
     }
 }
