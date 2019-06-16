09b218dadce2bb7bdd6ed34970a9a3ca48f2101d
kallard <contact@allard-kevin.fr>
2019-06-16 15:50:58 +0200

    :construction: Work in progress.

---
+++ app/src/main/java/fr/area42/weatherandroid/SplashActivity.kt
@@ -0,0 +1,45 @@
+package fr.area42.weatherandroid
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import fr.area42.weatherandroid.city.CityActivity
+
+class SplashActivity : AppCompatActivity() {
+    val PERMISSION_LOCATION_PHONE = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+
+        // Vérification des droits pour l'accès au GPS
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSION_LOCATION_PHONE)
+        } else {
+            cityActivity()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_LOCATION_PHONE -> {
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    cityActivity()
+                }
+            }
+        }
+    }
+
+    private fun cityActivity() {
+        val intent = Intent(this, CityActivity::class.java)
+        startActivity(intent)
+//        finish()
+    }
+}
