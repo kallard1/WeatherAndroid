d04674c31344647fa8f8fc7d4c8ae23ef415a1cd
kallard <contact@allard-kevin.fr>
2019-06-17 13:31:36 +0200

    feat: ajout de la verification de la connectivité internet

--- app/src/main/java/fr/area42/weatherandroid/SplashActivity.kt
+++ app/src/main/java/fr/area42/weatherandroid/SplashActivity.kt
@@ -1,28 +1,55 @@
 package fr.area42.weatherandroid
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.Snackbar
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import fr.area42.weatherandroid.city.CityActivity
+import java.util.*
+import kotlin.concurrent.schedule
 
 class SplashActivity : AppCompatActivity() {
     val PERMISSION_LOCATION_PHONE = 0
+    lateinit var coordinatorLayout: CoordinatorLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
 
-        // Vérification des droits pour l'accès au GPS
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSION_LOCATION_PHONE)
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        val isConnected = networkInfo?.isConnected ?: false
+
+        if (isConnected) {
+            // Vérification des droits pour l'accès au GPS
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSION_LOCATION_PHONE
+                )
+            } else {
+                cityActivity()
+            }
         } else {
-            cityActivity()
+            coordinatorLayout = findViewById(R.id.coordinator_layout)
+
+            val snackbar = Snackbar.make(coordinatorLayout, getString(R.string.settings_not_connected), Snackbar.LENGTH_INDEFINITE)
+             snackbar.setAction(getString(R.string.close)) {
+                 finish()
+             }
+            snackbar.show()
         }
     }
 
