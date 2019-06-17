9c380c2f39faaea13fe0d4ca16b542b2363634d4
kallard <contact@allard-kevin.fr>
2019-06-17 09:11:03 +0200

    style: suppression import inutile

--- app/src/main/java/fr/area42/weatherandroid/city/CityActivity.kt
+++ app/src/main/java/fr/area42/weatherandroid/city/CityActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
-import android.view.Menu
 import fr.area42.weatherandroid.R
 import fr.area42.weatherandroid.weather.WeatherActivity
 import fr.area42.weatherandroid.weather.WeatherFragment
