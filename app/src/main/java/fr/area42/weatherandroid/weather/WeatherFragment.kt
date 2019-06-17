277954590659caa2154799441a44d9828eaacdc9
kallard <contact@allard-kevin.fr>
2019-06-17 11:13:36 +0200

    feat: ajout de la toolbar + affichage du nom de la ville + bouton retour

--- app/src/main/java/fr/area42/weatherandroid/weather/WeatherFragment.kt
+++ app/src/main/java/fr/area42/weatherandroid/weather/WeatherFragment.kt
@@ -3,6 +3,8 @@ package fr.area42.weatherandroid.weather
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -32,8 +34,8 @@ class WeatherFragment : Fragment() {
     private lateinit var cityName: String
     private lateinit var locale: String
 
+    private lateinit var toolbar: Toolbar
     private lateinit var swipeRefresh: SwipeRefreshLayout
-    private lateinit var city: TextView
     private lateinit var weatherIcon: ImageView
     private lateinit var weatherDescription: TextView
     private lateinit var temperature: TextView
@@ -43,14 +45,17 @@ class WeatherFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_weather, container, false)
 
+        toolbar = view.findViewById(R.id.toolbar)
         swipeRefresh = view.findViewById(R.id.swipe_refresh)
-        city = view.findViewById(R.id.city)
-        weatherIcon = view.findViewById<ImageView>(R.id.weather_icon)
+        weatherIcon = view.findViewById(R.id.weather_icon)
         weatherDescription = view.findViewById(R.id.weather_description)
         temperature = view.findViewById(R.id.temperature_data)
         humidity = view.findViewById(R.id.humidity_data)
         pressure = view.findViewById(R.id.pressure_data)
         locale = Locale.getDefault().language
+
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         swipeRefresh.setOnRefreshListener { refreshWeather() }
 
@@ -73,36 +78,39 @@ class WeatherFragment : Fragment() {
         }
 
         val call = App.weatherService.getWeather("$cityName,fr", locale)
-        call.enqueue(object: Callback<WeatherWrapper> {
+        call.enqueue(object : Callback<WeatherWrapper> {
             override fun onResponse(call: Call<WeatherWrapper>?, response: Response<WeatherWrapper>?) {
                 response?.body()?.let {
                     val weather = mapOpenWeatherDataToWeather(it)
                     updateUi(weather)
                 }
-                Log.i(TAG, "OpenWeatherMap response: ${response?.body()}")
                 swipeRefresh.isRefreshing = false
             }
 
             override fun onFailure(call: Call<WeatherWrapper>, t: Throwable) {
                 Log.e(TAG, getString(R.string.could_not_load_city_weather), t)
-                Toast.makeText(activity,
+                Toast.makeText(
+                    activity,
                     getString(R.string.could_not_load_city_weather),
-                    Toast.LENGTH_SHORT).show()
+                    Toast.LENGTH_SHORT
+                ).show()
                 swipeRefresh.isRefreshing = false
             }
         })
     }
 
     private fun updateUi(weather: Weather) {
-
         Picasso.get()
             .load(weather.iconUrl)
             .placeholder(R.drawable.cloud_off_outline)
             .into(weatherIcon)
 
-        city.text = cityName
+        toolbar.title = cityName
         weatherDescription.text = weather.description
-        temperature.text = getString(R.string.weather_celsius_temperature_value, execute(weather.temperature, Convert.ToCelsius).toDouble())
+        temperature.text = getString(
+            R.string.weather_celsius_temperature_value,
+            execute(weather.temperature, Convert.ToCelsius).toDouble()
+        )
         humidity.text = getString(R.string.weather_humidity_value, weather.humidity)
         pressure.text = getString(R.string.weather_pressure_value, weather.pressure)
     }
