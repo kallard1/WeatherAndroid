9b728abf11ae3bfff3cd3387d298dee7bc5cbf20
kallard <contact@allard-kevin.fr>
2019-06-17 11:12:17 +0200

    style: suppression des logs

--- app/src/main/java/fr/area42/weatherandroid/weather/WeatherActivity.kt
+++ app/src/main/java/fr/area42/weatherandroid/weather/WeatherActivity.kt
@@ -2,13 +2,11 @@ package fr.area42.weatherandroid.weather
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 
 class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Log.d("WeatherActivity", "OnCreate()")
         supportFragmentManager.beginTransaction()
             .replace(android.R.id.content, WeatherFragment.newInstance())
             .commit()
